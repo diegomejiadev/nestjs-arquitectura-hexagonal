9b5ddeb77ee952a9477b0fe05ae15d8056ddb5c5
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-02-09 21:04:26 -0500

    Added Environment Variables Validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { TweetModule } from './modules/tweet/tweet.module';
+import { validate } from './core/env.validation';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: ['.env'],
+      validate,
     }),
     DatabaseModule,
     TweetModule,
---
+++ src/core/env.validation.ts
@@ -0,0 +1,31 @@
+import { plainToInstance } from 'class-transformer';
+import { IsNumber, IsString, validateSync } from 'class-validator';
+
+export enum Environment {
+  Development = 'development',
+  Production = 'production',
+  Test = 'test',
+  Provision = 'provision',
+}
+
+class EnvironmentVariables {
+  @IsNumber()
+  NESTJS_PORT: number;
+
+  @IsString()
+  NESTJS_MONGO_URI: string;
+}
+
+export function validate(config: Record<string, unknown>) {
+  const validatedConfig = plainToInstance(EnvironmentVariables, config, {
+    enableImplicitConversion: true,
+  });
+  const errors = validateSync(validatedConfig, {
+    skipMissingProperties: false,
+  });
+
+  if (errors.length > 0) {
+    throw new Error(errors.toString());
+  }
+  return validatedConfig;
+}
